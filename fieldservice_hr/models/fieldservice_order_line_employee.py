e3c30d93d35650a1fc522b2ad5779cdd9b85191d
Marcus Jakobsson <markusjacobsson93@gmail.com>
2025-03-10 09:55:23 +0100

    Update fieldservice_order_line_employee.py

--- fieldservice_hr/models/fieldservice_order_line_employee.py
+++ fieldservice_hr/models/fieldservice_order_line_employee.py
@@ -5,8 +5,8 @@ class FieldServiceOrderLineEmployee(models.Model):
     _description = 'Field Service Order Line Employee'
 
 
-    fieldservice_order_line_id = fields.Many2one('fieldservice.order.line')
-    employee_id = fields.Many2one('hr.employee')
+    fieldservice_order_line_id = fields.Many2one('fieldservice.order.line', required=True)
+    employee_id = fields.Many2one('hr.employee', required=True)
     # order_ids = fields.One2many('fieldservice.order.line','order_id', string='Service Order', required=True)
     description = fields.Text(string='Description')
     name = fields.Char(compute="compute_name", store=True, readonly=False)
