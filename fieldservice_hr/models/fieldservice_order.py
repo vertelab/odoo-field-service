e6c7dd0500fbbf64a2890256cc8edbca0ef803dc
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2025-04-07 14:16:21 +0000

    fixes

--- fieldservice_hr/models/fieldservice_order.py
+++ fieldservice_hr/models/fieldservice_order.py
@@ -4,10 +4,11 @@ class FieldServiceOrder(models.Model):
     _inherit = 'fieldservice.order'
 
     total_time_duration = fields.Float(string='Total Time Duration',compute='_compute_total_time_duration',store=True)
-
+    total_time_duration_avg = fields.Float(string='Total Time Duration AVG',compute='_compute_total_time_duration',store=True,group_operator="avg")
     @api.depends('order_line_ids.timesheet_ids.unit_amount')
     def _compute_total_time_duration(self):
         for record in self:
             record.total_time_duration = sum(
                 record.order_line_ids.mapped('timesheet_ids').mapped('unit_amount')
             )
+            record.total_time_duration_avg = record.total_time_duration
--- fieldservice_vrtl/models/fieldserviceorder.py
+++ fieldservice_vrtl/models/fieldserviceorder.py
@@ -73,9 +73,9 @@ class FieldServiceOrder(models.Model):
     purchase_date = fields.Date(string='Purchase Date', help="The date when the product was purchased")
 
     #ocassion count
-    line_count = fields.Integer(string='Line Count', compute='_compute_line_count', store = True)
-    line_count_avg = fields.Integer(group_operator='avg',string='Line Count (AVG)', compute='_compute_line_count', store=True)
-    total_duration = fields.Float(string='Total Duration', compute='_compute_total_duration', store=True, help='Total duration of all lines')
+    line_count = fields.Integer(string='Occasion Count', compute='_compute_line_count', store = True)
+    line_count_avg = fields.Integer(group_operator='avg',string='Occasion Count (AVG)', compute='_compute_line_count', store=True)
+    total_duration = fields.Float(string='Total Duration', compute='_compute_total_duration', store=True, help='Total duration of all lines', group_operator=False)
 
     @api.depends('order_line_ids.duration')
     def _compute_total_duration(self):
