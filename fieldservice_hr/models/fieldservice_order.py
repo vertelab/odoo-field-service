bb9af105501aa868c417f3b92612b41ed8589153
vertelbot <vertelbot@vertel.se>
2025-06-13 07:28:28 +0000

    odoobranchpfile odoo-field-service from 18.0. Do not p-file sync

--- fieldservice_hr/models/fieldservice_order.py
+++ fieldservice_hr/models/fieldservice_order.py
@@ -4,7 +4,8 @@ class FieldServiceOrder(models.Model):
     _inherit = 'fieldservice.order'
 
     total_time_duration = fields.Float(string='Total Time Duration',compute='_compute_total_time_duration',store=True)
-    total_time_duration_avg = fields.Float(string='Total Time Duration AVG',compute='_compute_total_time_duration',store=True,group_operator="avg")
+    total_time_duration_avg = fields.Float(
+        string='Total Time Duration AVG', compute='_compute_total_time_duration', store=True, aggregator="avg")
     
     @api.depends('order_line_ids.timesheet_ids.unit_amount')
     def _compute_total_time_duration(self):
